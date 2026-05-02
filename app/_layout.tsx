b1b6db0d672f0b160bb700a6e6fc7a46637f1b03
azhnhakimi <azhanhakimii@gmail.com>
2026-05-02 21:38:28 +0800

    added fonts and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,39 @@
 import { ThemeProvider } from "@/theme/ThemeProvider";
-import { Slot } from "expo-router";
+import {
+  SpaceGrotesk_300Light,
+  SpaceGrotesk_400Regular,
+  SpaceGrotesk_500Medium,
+  SpaceGrotesk_600SemiBold,
+  SpaceGrotesk_700Bold,
+  useFonts,
+} from "@expo-google-fonts/space-grotesk";
+import { Slot, SplashScreen } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceGrotesk_300Light,
+    SpaceGrotesk_400Regular,
+    SpaceGrotesk_500Medium,
+    SpaceGrotesk_600SemiBold,
+    SpaceGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded) return null;
+
   return (
     <ThemeProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Slot />
+        <StatusBar style="dark" />
       </GestureHandlerRootView>
     </ThemeProvider>
   );
---
+++ constants/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  light: "SpaceGrotesk_300Light",
+  regular: "SpaceGrotesk_400Regular",
+  medium: "SpaceGrotesk_500Medium",
+  semibold: "SpaceGrotesk_600SemiBold",
+  bold: "SpaceGrotesk_700Bold",
+};
